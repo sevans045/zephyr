0cf581264ebcf99500a8316a7627c68796c3246c
Sarah Evans <sarahevans045@gmail.com>
2018-09-04 15:44:10 -0500

    Remove unused chalk colors

--- src/message-handling/Log.ts
+++ src/message-handling/Log.ts
@@ -4,16 +4,6 @@ import * as chalk from 'chalk';
 export class Log {
 
   private static c = chalk.default;
-  // Set logging colors
-  // c.setTheme({
-  //   data: "cyan",
-  //   log: "yellow",
-  //   debug: "blue",
-  //   error: "red",
-  //   debuf: "white",
-  //   green: "green",
-  //   GDI: "yellow"
-  // });
   
   public static data (message?: any, ...optionalParams: any[]):void {
     console.info(this.c.cyan(message), optionalParams);
@@ -31,15 +21,8 @@ export class Log {
     console.error(this.c.red(message), optionalParams);
   }
 
-  public static debuf (message?: any, ...optionalParams: any[]):void {
-    console.info(this.c.white(message), optionalParams);
-  }
-
   public static green (message?: any, ...optionalParams: any[]):void {
     console.info(this.c.green(message), optionalParams);
   }
 
-  public static GDI (message?: any, ...optionalParams: any[]):void {
-    console.info(this.c.yellow(message), optionalParams);
-  }
 }
